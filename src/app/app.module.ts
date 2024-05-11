4348b1e5274f66e490ad44e0b389490b21209f2b
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-11 16:17:51 +0300

    TEST Flatpickr directive was generated with "ng g directive directives/flatpickr"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvoiceCreateComponent } from './invoices/invoice-create/invoice-create.component';
+import { FlatpickrDirective } from './invoices/invoice-create/flatpickr.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InvoiceCreateComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InvoiceCreateComponent, FlatpickrDirective],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/directives/flatpickr.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appFlatpickr]'
+})
+export class FlatpickrDirective {
+
+  constructor() { }
+
+}
