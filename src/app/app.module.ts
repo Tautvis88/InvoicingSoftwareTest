44c0547ca94238fada83ee3a7552f514cf2f2f0a
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-11 16:22:03 +0300

    TEST Set max_line_length to 120 for Prettier formatting tool, deleted unsupported config line quote_type=single, app.modules.ts import fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InvoiceCreateComponent } from './invoices/invoice-create/invoice-create.component';
-import { FlatpickrDirective } from './invoices/invoice-create/flatpickr.directive';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { InvoiceCreateComponent } from "./invoices/invoice-create/invoice-create.component";
+import { FlatpickrDirective } from "./directives/flatpickr.directive";
 
 @NgModule({
   declarations: [AppComponent, InvoiceCreateComponent, FlatpickrDirective],
