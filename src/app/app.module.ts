2df8b708ba5b3fd1bca711bc725c27a425260947
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-12 12:06:06 +0300

    TEST Created JSON file, added json parsing logic to buyer.service.ts, imported HttpClientModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { InvoiceCreateComponent } from "./invoices/invoice-create/invoice-create.component";
 import { FlatpickrDirective } from "./directives/flatpickr.directive";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, InvoiceCreateComponent, FlatpickrDirective],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/services/buyer.service.ts
+++ src/app/services/buyer.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export interface Buyer {
+  name: string;
+  address: string;
+  companyCode: string;
+  VATCode: string;
+  phoneNumber: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class BuyerService {
-  constructor() {}
+  private readonly jsonUrl: string = "assets/buyer-list.json";
+  constructor(private http: HttpClient) {}
+
+  getBuyers(): Observable<Buyer[]> {
+    return this.http.get<Buyer[]>(this.jsonUrl);
+  }
 }
