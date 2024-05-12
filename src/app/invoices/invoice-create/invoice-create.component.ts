13820671126ad03e9f9db69ba47f31b27815a418
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-12 13:05:21 +0300

    TEST Added code that populates the buyer's dropdown and reacts when the dropdown value changes

--- src/app/invoices/invoice-create/invoice-create.component.ts
+++ src/app/invoices/invoice-create/invoice-create.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Buyer, BuyerService } from "../../services/buyer.service";
 
 @Component({
-  selector: 'app-invoice-create',
-  templateUrl: './invoice-create.component.html',
-  styleUrl: './invoice-create.component.css'
+  selector: "app-invoice-create",
+  templateUrl: "./invoice-create.component.html",
+  styleUrl: "./invoice-create.component.css",
 })
-export class InvoiceCreateComponent {
+export class InvoiceCreateComponent implements OnInit {
+  buyers: Buyer[] = [];
+  selectedBuyer: Buyer | null = null;
 
+  constructor(private buyerService: BuyerService) {}
+
+  ngOnInit(): void {
+    this.loadBuyers();
+  }
+
+  loadBuyers(): void {
+    this.buyerService.getBuyers().subscribe((data: Buyer[]) => {
+      this.buyers = data;
+    });
+  }
+
+  onBuyerChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedName = selectElement.value;
+    this.selectedBuyer = this.buyers.find((buyer) => buyer.name === selectedName) || null;
+  }
 }
