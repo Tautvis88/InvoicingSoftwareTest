b3c40f97eb40cfcb8059c55cdaa745d467a3b17e
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-24 16:06:53 +0300

    TEST Replaced default HTML5 dropdown <datalist/> with PrimeNG editable dropdown and added required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { InvoiceCreateComponent } from "./invoices/invoice-create/invoice-create
 import { FlatpickrDirective } from "./directives/flatpickr.directive";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { DropdownModule } from "primeng/dropdown";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, InvoiceCreateComponent, FlatpickrDirective],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, DropdownModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
