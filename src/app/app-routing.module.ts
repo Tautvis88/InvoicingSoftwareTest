61dfedbd9769ae684cc059a12da1b9b2f3b62b7e
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-28 12:54:14 +0300

    TEST Added routing paths and <router-outlet> component to make sidebar buttons work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { InvoiceListComponent } from "./invoices/invoice-list/invoice-list.component";
+import { InvoiceCreateComponent } from "./invoices/invoice-create/invoice-create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "invoices", component: InvoiceListComponent },
+  { path: "create-invoice", component: InvoiceCreateComponent },
+  { path: "", redirectTo: "/invoices", pathMatch: "full" }, //Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
