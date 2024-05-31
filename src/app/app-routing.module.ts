29b31532771f4e0cbe926ab36c21dd3b9183a0f3
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-31 17:21:01 +0300

    TEST Added new routing paths, updated sidebar.component button with routerLink path.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { InvoiceListComponent } from "./invoices/invoice-list/invoice-list.component";
 import { InvoiceCreateComponent } from "./invoices/invoice-create/invoice-create.component";
+import { AddClientComponent } from "./invoices/add-client/add-client.component";
 
 const routes: Routes = [
   { path: "invoices", component: InvoiceListComponent },
   { path: "create-invoice", component: InvoiceCreateComponent },
+  { path: "add-client", component: AddClientComponent },
   { path: "", redirectTo: "/invoices", pathMatch: "full" }, //Default route
+  { path: "**", redirectTo: "/invoices" },
 ];
 
 @NgModule({
